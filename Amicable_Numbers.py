91eb325babaa3a51fed90555d8eaaa6268c5027f
Shilpabhavani1 <96565325+Shilpabhavani1@users.noreply.github.com>
2022-10-31 14:51:24 +0530

    def get_pfs(n):     s=0     for i in range(1,n):         if n%i==0:             s += i     return s
    a=int(input())
    b=int(input())
    if get_pfs(a)==b and get_pfs(b)==a:
        print("Amicable")
    else:
        print("Not Amicable");

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,17 +1,17 @@
+def get_pfs(n):
+    s=0
+    for i in range(1,n):
+        if n%i==0:
+            s += i
+    return s
+    
 a=int(input())
 b=int(input())
-s1=0
-s2=0
-for i in range(1,a):
-    if a%i==0:
-        s1 += i
-for i in range(1,b):
-    if b%i==0:
-        s2 += i
-if s1==b and s2==a:
+if get_pfs(a)==b and get_pfs(b)==a:
     print("Amicable")
 else:
     print("Not Amicable");
      
        
   
+
